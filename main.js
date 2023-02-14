34572e0a0b5eaeedab910d689f16ca70a320c5e9
BondarAndrew <107639998+BondarAndrew@users.noreply.github.com>
2023-02-14 21:51:24 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+//Ex1
+for (i = 3; i <= 20; i++) {
+  console.log(i);
+}
+
+//Ex2
+for (i2 = 10; i2 <= 20; i2++) {
+  console.log(i2);
+}
+
+//Ex3
+let number1 = prompt("Enter");
+let number2 = prompt("Enter");
+let n1 = Number(number1);
+let n2 = Number(number2);
+for (i = n1; i <= n2; i++) {
+  console.log(i);
+}
+
+// h version let number1 = prompt("Enter");
+// let number2 = prompt("Enter");
+// let n1 = Number(number1);
+// let n2 = Number(number2);
+// for (i = n1 + 1; i < n2; i++) {
+//   console.log(i);
+// }
